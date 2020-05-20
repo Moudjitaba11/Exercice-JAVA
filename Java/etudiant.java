7d3b1e23622bd52c3e632dfac5c6cf41e4794ce4
Moudjitaba Barry <moudjitaba69@gmail.com>
2020-05-20 14:10:27 +0000

    Mon projet java

---
+++ Java/etudiant.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+LocalDate localdate=new LocalDate.now();
+public class Etudiant extends Personne{
+
+	private String tuteur;
+    private String datenaiss;
+	
+    this.fullname=fullname;
+	
+    public Etudiant(String fullname){
+
+}
+	public String getDateNaiss(){
+
+		return datenaiss;
+	}
+	public String setDateNaiss(String datenaiss){
+
+		this.datenaiss=datenaiss;
+	}
+
+	public String getTuteur(){
+
+	return tuteur;
+	}
+
+	public String setTuteur(String tuteur){
+
+	this.tuteur=tuteur;
+	}
+
+	
+	public int CalculDate(){
+
+		
+	}
